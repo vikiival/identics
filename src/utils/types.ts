390c4b11d589c8a5ff099ea209647a1d2e3a75de
Viki Val <viktorko99@gmail.com>
2025-02-22 00:07:52 +0100

    :add: add types and context for Substrate batch processing

---
+++ src/utils/types.ts
@@ -0,0 +1,85 @@
+import {
+  DataHandlerContext,
+  SubstrateBatchProcessorFields,
+  Block as _Block,
+  Call as _Call,
+  Event as _Event,
+  Extrinsic as _Extrinsic,
+  type SubstrateBatchProcessor as SubstrateProcessor,
+  BlockHeader
+} from '@subsquid/substrate-processor'
+
+import { Store as SquidStore } from '@subsquid/typeorm-store'
+import { EntityManager } from 'typeorm'
+import { Interaction } from '../model'
+
+export type BaseCall = {
+  caller: string
+  blockNumber: string
+  timestamp: Date
+  name?: string
+}
+
+
+export const fieldSelection = {
+  block: {
+    timestamp: true
+  },
+  extrinsic: {
+    signature: true,
+  },
+  call: {
+      name: true,
+      args: true,
+      origin: true,
+      extrinsic: true
+  },
+  event: {
+      name: true,
+      args: true,
+  }
+} as const
+
+export type SelectedFields = typeof fieldSelection
+
+type Fields = SubstrateBatchProcessorFields<SubstrateProcessor<SelectedFields>>
+export type Block = _Block<Fields>
+export type Event = _Event<Fields>
+export type Call = _Call<Fields>
+export type Extrinsic = _Extrinsic<Fields>
+
+export type ManagedStore = SquidStore & { em: () => EntityManager }
+export type Store =  SquidStore // & { em: () => EntityManager }
+export type BatchContext<S = Store> = DataHandlerContext<S, Fields>
+export type SelectedBlock = Pick<BlockHeader<Fields>, 'height' | 'timestamp' | 'hash'>
+export type SelectedEvent = Pick<Event, 'name' | 'args'>
+export type SelectedExtrinsic = Pick<Extrinsic, 'signature' | 'call'>
+export type SelectedCall = Pick<Call, 'name' | 'origin' | 'extrinsic' | 'args' | 'block'>
+
+export type Context<S = Store>  = {
+  store: S
+  block: SelectedBlock
+  event: SelectedEvent
+  extrinsic: SelectedExtrinsic | undefined
+  call: SelectedCall
+  // log: Logger
+}
+
+export type Optional<T> = T | null
+
+export interface IEvent<T extends Interaction> {
+  interaction: T
+  blockNumber: bigint
+  caller: string
+  currentOwner: string
+  timestamp: Date
+  meta: string
+}
+
+export type CallWith<T> = BaseCall & T
+
+export type EntityConstructor<T> = {
+  new (...args: any[]): T
+}
+
+export { Interaction as Action } from '../model'
