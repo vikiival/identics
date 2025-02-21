4ecf02c976c11af335895197541c265540a85265
Viki Val <viktorko99@gmail.com>
2025-02-22 00:08:55 +0100

    :modify: update processor to use people-chain and add identity-related calls

--- src/processor.ts
+++ src/processor.ts
@@ -9,27 +9,60 @@ import {
     Extrinsic as _Extrinsic
 } from '@subsquid/substrate-processor'
 
-import {events} from './types/polkadot'
+import {events} from './types/people'
+import {calls} from './types/people'
 
-const ARCHIVE_URL = `https://v2.archive.subsquid.io/network/polkadot`
-// const NODE_URL = `wss://${CHAIN}-asset-hub-rpc.polkadot.io`
+const ARCHIVE_URL = `https://v2.archive.subsquid.io/network/people-chain`
+const NODE_URL = `wss://sys.ibp.network/people-polkadot`
+const STARTING_BLOCK = 188868
+
+console.log(Object.keys(events.identity))
 
 export const processor = new SubstrateBatchProcessor()
+    .setBlockRange({ from: STARTING_BLOCK - 8e3 })
     // Lookup archive by the network name in Subsquid registry
     // See https://docs.subsquid.io/substrate-indexing/supported-networks/
-    .setGateway('https://v2.archive.subsquid.io/network/polkadot')
+    .setGateway(ARCHIVE_URL)
     // Chain RPC endpoint is required on Substrate for metadata and real-time updates
     .setRpcEndpoint({
         // Set via .env for local runs or via secrets when deploying to Subsquid Cloud
         // https://docs.subsquid.io/deploy-squid/env-variables/
-        url: assertNotNull(process.env.NODE_URL, 'No RPC endpoint supplied'),
+        url: assertNotNull(NODE_URL, 'No RPC endpoint supplied'),
         // More RPC connection options at https://docs.subsquid.io/substrate-indexing/setup/general/#set-data-source
         rateLimit: 10
     })
-    .addEvent({
-        name: [events.identity.identitySet.name],
-        extrinsic: true
+    .addCall({
+        name: [calls.identity.setIdentity.name],
+        extrinsic: true,
     })
+    .addCall({
+        name: [calls.identity.provideJudgement.name],
+        extrinsic: true,
+    })
+    .addCall({
+        name: [calls.identity.addSub.name],
+        extrinsic: true,
+    })
+    .addCall({
+        name: [calls.identity.setSubs.name],
+        extrinsic: true,
+    })
+    .addCall({
+        name: [calls.identity.renameSub.name],
+        extrinsic: true,
+    })
+    .addCall({
+        name: [calls.identity.addUsernameAuthority.name],
+        extrinsic: true,
+    })
+    .addCall({
+        name: [calls.identity.removeUsernameAuthority.name],
+        extrinsic: true,
+    })
+    // .addEvent({
+    //     name: [events.identity.identitySet.name],
+    //     extrinsic: true
+    // })
     .setFields({
         event: {
             args: true
@@ -41,9 +74,10 @@ export const processor = new SubstrateBatchProcessor()
         block: {
             timestamp: true
         }
-    })
+    });
     // Uncomment to disable RPC ingestion and drastically reduce no of RPC calls
-    .useArchiveOnly()
+    processor.setRpcDataIngestionSettings({ disabled: true })
+
 
 export type Fields = SubstrateBatchProcessorFields<typeof processor>
 export type Block = BlockHeader<Fields>
