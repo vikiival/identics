099049e7a1ef48107d4ee580464286c534c3bdfe
Viki Val <viktorko99@gmail.com>
2025-03-07 21:55:05 +0100

    refactor: clean up handleJudgementProvide function and remove commented code

--- src/mapping/identities/provideJudgement.ts
+++ src/mapping/identities/provideJudgement.ts
@@ -4,7 +4,9 @@ import { unwrap } from "../../utils/extract"
 import { debug, pending, success } from "../../utils/logger"
 import { Action, Context } from "../../utils/types"
 import { getProvideJudgementCall, getSetIdentityCall } from "../getters"
-import { Identity } from '../../model'
+import { create, getOrFail as get, findByIdListAsMap as getMany } from '@kodadot1/metasquid/entity'
+import { Identity, Judgement } from '../../model'
+
 
 const OPERATION = `CALL::JUDGEMENT` //Action.CREATE
 
@@ -15,31 +17,22 @@ const OPERATION = `CALL::JUDGEMENT` //Action.CREATE
  * @param context - the context for the Call   
  **/
 export async function handleJudgementProvide(context: Context): Promise<void> {
-    pending(OPERATION, `[COLECTTION++]: ${context.block.height}`)
+    pending(OPERATION, `${context.block.height}`)
     const call = unwrap(context, getProvideJudgementCall)
     debug(OPERATION, call)
 
     const id = call.caller
-    // const final = await getOrCreate(context.store, Identity, id, {})
+    // const final = await get(context.store, Identity, id)
+
+    // final.jugdement = call.judgement as Judgement
+    // final.registrar = call.registrarId
+    // final.hash = call.checksum
 
     // Set properties from basic
     // final.blockNumber = BigInt(call.blockNumber)
     // final.createdAt = call.timestamp
     // final.updatedAt = call.timestamp
 
-    // Set properties from IdentityInfo
-    // final.name = call.display
-    // final.legal = call.legal
-    // final.web = call.web
-    // final.matrix = call.matrix
-    // final.email = call.email
-    // final.image = call.image
-    // final.twitter = call.twitter
-    // final.github = call.github
-    // final.discord = call.discord
-
-    // success(OPERATION, `[COLLECTION] ${final.id}`)
+    // success(OPERATION, `${final.id}/${final.jugdement}//${final.jugdement}`)
     // await context.store.save(final)
-
-    console.log(`Identity set to: ${id}`)
 }
