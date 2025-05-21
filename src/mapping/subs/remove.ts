151149264e935aa92b040df54d3791d59b524874
Viki Val <viktorko99@gmail.com>
2025-05-21 18:50:34 +0200

    :sparkles: implementation in src/mapping/subs/remove.ts

--- src/mapping/subs/remove.ts
+++ src/mapping/subs/remove.ts
@@ -1,7 +1,9 @@
+import { get } from '@kodadot1/metasquid/entity'
+import { Sub } from '../../model'
 import { unwrap } from '../../utils/extract'
-import { debug, pending } from '../../utils/logger'
+import { debug, pending, skip, success } from '../../utils/logger'
 import { Context } from '../../utils/types'
-import { getRemoveSubCall } from '../getters'
+import { getRemoveSubCall, getSubIdentityRemovedEvent } from '../getters'
 
 const OPERATION = `CALL::REMOVE_SUB` //Action.CREATE
 
@@ -13,30 +15,16 @@ const OPERATION = `CALL::REMOVE_SUB` //Action.CREATE
  */
 export async function handleSubRemove(context: Context): Promise<void> {
   pending(OPERATION, `${context.block.height}`)
-  const call = unwrap(context, getRemoveSubCall)
-  debug(OPERATION, call)
+  const event = unwrap(context, getSubIdentityRemovedEvent)
+  debug(OPERATION, event)
 
-  const id = call.caller
-  // const final = await getOrCreate(context.store, Identity, id, {})
+  const id = event.sub
+  const final = await get(context.store, Sub, id)
+  if (!final) {
+    skip(OPERATION, `Sub not found: ${id}`)
+    return
+  }
 
-  // Set properties from basic
-  // final.blockNumber = BigInt(call.blockNumber)
-  // final.createdAt = call.timestamp
-  // final.updatedAt = call.timestamp
-
-  // Set properties from IdentityInfo
-  // final.name = call.display
-  // final.legal = call.legal
-  // final.web = call.web
-  // final.matrix = call.matrix
-  // final.email = call.email
-  // final.image = call.image
-  // final.twitter = call.twitter
-  // final.github = call.github
-  // final.discord = call.discord
-
-  // success(OPERATION, `[COLLECTION] ${final.id}`)
-  // await context.store.save(final)
-
-  console.log(`Identity set to: ${id}`)
+  success(OPERATION, `${event.main}/${id}`)
+  await context.store.remove(final)
 }
