a6fbaff3fb516f52102a65facf13cbdd619e21ce
Viki Val <viktorko99@gmail.com>
2025-05-21 18:51:18 +0200

    :sparkles: implementation in src/mapping/usernames/kill.ts

---
+++ src/mapping/usernames/kill.ts
@@ -0,0 +1,34 @@
+import { get, getOrCreate } from '@kodadot1/metasquid/entity'
+
+import { unwrap } from '../../utils/extract'
+import { debug, pending, success } from '../../utils/logger'
+import { Action, Context } from '../../utils/types'
+import {
+  getAddSubCall,
+  getSetIdentityCall,
+  getSetSubsCall,
+  getUsernameSetEvent,
+} from '../getters'
+import { Identity, Username } from '../../model'
+
+const OPERATION = `CALL::SET_USERNAME` //Action.CREATE
+
+/**
+ * Handle the identity create call (Identity.set_identity)
+ * Creates a new Identity entity
+ * Logs Action.CREATE event
+ * @param context - the context for the Call
+ */
+export async function handleUsernameKill(context: Context): Promise<void> {
+  pending(OPERATION, `${context.block.height}`)
+  const event = unwrap(context, getUsernameSetEvent)
+  debug(OPERATION, event)
+
+  const id = event.username
+  const final = await getOrCreate(context.store, Username, id, {})
+
+  final.createdAt = event.timestamp
+
+  const identity = await get(context.store, Identity, event.who)
+  final.identity = identity
+}
