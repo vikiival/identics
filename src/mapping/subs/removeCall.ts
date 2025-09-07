d7f2ffd85f318c66a9321c4a56d03826579a9b20
Viki Val <viktorko99@gmail.com>
2025-09-07 19:16:01 +0300

    :broom: naming in src/mapping/subs/removeCall.ts

--- src/mapping/subs/removeCall.ts
+++ src/mapping/subs/removeCall.ts
@@ -3,28 +3,28 @@ import { Sub } from '../../model'
 import { unwrap } from '../../utils/extract'
 import { debug, pending, skip, success } from '../../utils/logger'
 import { Context } from '../../utils/types'
-import { getRemoveSubCall, getSubIdentityRemovedEvent } from '../getters'
+import { getRemoveSubCall } from '../getters'
 
 const OPERATION = `CALL::REMOVE_SUB` //Action.CREATE
 
 /**
- * Handle the identity create call (Identity.set_identity)
- * Creates a new Identity entity
- * Logs Action.CREATE event
+ * Handle the sub-identity remove call (Identity.remove_sub)
+ * Removes existing Sub entity
+ * Logs CALL::REMOVE_SUB call
  * @param context - the context for the Call
  */
 export async function handleSubRemoveCall(context: Context): Promise<void> {
   pending(OPERATION, `${context.block.height}`)
-  const event = unwrap(context, getRemoveSubCall)
-  debug(OPERATION, event)
+  const call = unwrap(context, getRemoveSubCall)
+  debug(OPERATION, call)
 
-  const id = event.sub
+  const id = call.sub
   const final = await get(context.store, Sub, id)
   if (!final) {
     skip(OPERATION, `Sub not found: ${id}`)
     return
   }
 
-  success(OPERATION, `${event.main}/${id}`)
+  success(OPERATION, `${call.main}/${id}`)
   await context.store.remove(final)
 }
