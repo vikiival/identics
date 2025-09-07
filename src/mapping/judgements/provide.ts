941c0adeafca9e7ab29a2b88a150166376ca9d26
Viki Val <viktorko99@gmail.com>
2025-09-07 19:16:00 +0300

    :broom: naming in src/mapping/judgements/provide.ts

--- src/mapping/judgements/provide.ts
+++ src/mapping/judgements/provide.ts
@@ -1,16 +1,16 @@
 import { get } from '@kodadot1/metasquid/entity'
+import { Identity, Judgement } from '../../model'
 import { unwrap } from '../../utils/extract'
 import { debug, pending, skip, success } from '../../utils/logger'
 import { Context } from '../../utils/types'
 import { getProvideJudgementCall } from '../getters'
-import { Identity, Judgement } from '../../model'
 
-const OPERATION = `CALL::JUDGEMENT_GIVE` //Action.CREATE
+const OPERATION = `CALL::JUDGEMENT_GIVE`
 
 /**
- * Handle the identity create call (Identity.set_identity)
- * Creates a new Identity entity
- * Logs Action.CREATE event
+ * Handle the identity create call (Identity.provide_judgement)
+ * Provides judgement for a given identity
+ * Logs CALL::JUDGEMENT_GIVE event
  * @param context - the context for the Call
  */
 export async function handleJudgementProvide(context: Context): Promise<void> {
