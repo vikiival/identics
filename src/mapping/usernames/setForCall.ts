8f3ec0d42a5f467df399eee423066b47a041507a
Viki Val <viktorko99@gmail.com>
2025-09-08 19:12:37 +0300

    :technologist: small updates  src/mapping/usernames/setForCall.ts

--- src/mapping/usernames/setForCall.ts
+++ src/mapping/usernames/setForCall.ts
@@ -19,7 +19,7 @@ export async function handleUsernameSetForCall(
 ): Promise<void> {
   pending(OPERATION, `${context.block.height}`)
   const call = unwrap(context, getSetUsernameForCall)
-  debug(OPERATION, call)
+  debug(OPERATION, call, true)
 
   const id = call.username
   const final = await getOrCreate(context.store, Username, id, {})
