c6729fc6d503907fc6c8a7a57b72b5f06caddc8b
Viki Val <viktorko99@gmail.com>
2025-09-08 13:31:55 +0300

    :broom: naming in src/mapping/usernames/setForCall.ts

--- src/mapping/usernames/setForCall.ts
+++ src/mapping/usernames/setForCall.ts
@@ -6,30 +6,30 @@ import { debug, pending } from '../../utils/logger'
 import { Context } from '../../utils/types'
 import { getSetUsernameForCall } from '../getters'
 
-const OPERATION = `CALL::SET_USERNAME_FOR`
+const OPERATION = `CALL::USERNAME_SET_FOR`
 
 /**
- * Handle the identity create call (Identity.set_identity)
- * Creates a new Identity entity
- * Logs Action.CREATE event
+ * Handle the username set (for) call (Identity.set_username_for)
+ * Creates a new Username entity
+ * Logs CALL::SET_USERNAME_FOR call
  * @param context - the context for the Call
  */
 export async function handleUsernameSetForCall(
   context: Context
 ): Promise<void> {
   pending(OPERATION, `${context.block.height}`)
-  const event = unwrap(context, getSetUsernameForCall)
-  debug(OPERATION, event)
+  const call = unwrap(context, getSetUsernameForCall)
+  debug(OPERATION, call)
 
-  const id = event.username
+  const id = call.username
   const final = await getOrCreate(context.store, Username, id, {})
 
-  final.createdAt = event.timestamp
+  final.createdAt = call.timestamp
   final.primary = false
-  final.name = event.username
+  final.name = call.username
   final.status = UsernameStatus.Queued
 
-  const identity = await get(context.store, Identity, event.who)
+  const identity = await get(context.store, Identity, call.who)
   final.identity = identity
 
   await context.store.save(final)
