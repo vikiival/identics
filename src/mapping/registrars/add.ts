b075e96640059fb1e3fea89057e6664b3f1c0378
Viki Val <viktorko99@gmail.com>
2025-09-08 22:22:16 +0300

    feat(identity): refactor registrar addition to utilize storage info retrieval

--- src/mapping/registrars/add.ts
+++ src/mapping/registrars/add.ts
@@ -1,9 +1,11 @@
 import { create } from '@kodadot1/metasquid/entity'
 import { Registrar } from '../../model'
+
 import { unwrap } from '../../utils/extract'
+import { addressOf } from '../../utils/helper'
 import { debug, pending, success } from '../../utils/logger'
 import { Context } from '../../utils/types'
-import { getAddRegistrarCall, getRegistrarAddedEvent } from '../getters'
+import { getRegistrarAddedEvent, getRegistrarStorageInfo } from '../getters'
 
 const OPERATION = `CALL::ADD_REGISTRAR`
 
@@ -16,18 +18,31 @@ const OPERATION = `CALL::ADD_REGISTRAR`
 export async function handleRegistrarAdd(context: Context): Promise<void> {
   pending(OPERATION, `${context.block.height}`)
   const event = unwrap(context, getRegistrarAddedEvent)
-  const call = unwrap(context, getAddRegistrarCall)
-  debug(OPERATION, event)
-  debug(OPERATION, call, true)
+  debug(OPERATION, event, true)
+
+  const storageAccess = getRegistrarStorageInfo()
+  const storageInfo = await storageAccess
+    .get(context.block as any)
+    .then((info: any) => (info || []).filter(Boolean))
+  debug(OPERATION, storageInfo || {}, true)
+
+  const info = storageInfo.at(event.index)
+
+  if (!info) {
+    throw new Error(`No registrar info found at index ${event.index}`)
+  }
 
   const id = event.index.toString()
   const final = create(Registrar, id, {
-    address: call.account,
+    address: addressOf(info.account),
     blockNumber: BigInt(event.blockNumber),
-    createdAt: call.timestamp,
-    updatedAt: call.timestamp,
+    createdAt: event.timestamp,
+    updatedAt: event.timestamp,
+    fee: BigInt(info.fee),
+    field: BigInt(info.fields),
+    origin: event.origin,
   })
 
-  success(OPERATION, `OK`)
+  success(OPERATION, `${final.id}/${final.address}`)
   await context.store.save(final)
 }
