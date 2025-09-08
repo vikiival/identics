3d7547644ff174bdd83a4d1c19fddae5811fbe3c
Viki Val <viktorko99@gmail.com>
2025-09-08 15:23:45 +0300

    :add: add CountEntity and CountEntityQueryResult classes for GraphQL schema

---
+++ src/server-extension/model/count.model.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata'
+import { Field, ObjectType } from 'type-graphql'
+
+@ObjectType()
+export class CountEntity {
+  @Field(() => Number, { nullable: false, name: 'totalCount' })
+  total!: number
+
+  constructor(totalCount: number) {
+    this.total = totalCount
+  }
+}
+
+@ObjectType()
+export class CountEntityQueryResult {
+  @Field(() => Number, { nullable: false })
+  total_count!: number
+
+  constructor(props: Partial<CountEntityQueryResult>) {
+    Object.assign(this, props)
+  }
+}
